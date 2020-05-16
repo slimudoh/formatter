f3fa9fd9ad53c86d6afc4d32bf4017e013a36f82
uwem udoh <uwem.udoh@zedcrestcapital.cm>
2020-05-16 17:23:17 +0100

    updated

--- currency.js
+++ currency.js
@@ -1,34 +1,34 @@
-function formatCurrency() {
-  const value = this.amountSold.replace(/,/g, "");
-  if (+value) {
-    if (value.includes(".")) {
-      if (value.split(".")[1].length) {
-        const valueBeforeComma = value.split(".")[0];
-        const valueAfterComma = value.split(".")[1];
-        let numBeforeComma;
-        if (valueAfterComma.length <= 2) {
-          const numBeforeComma = value
-            .split(".")[0]
-            .toString()
-            .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-          this.amountSold = `${numBeforeComma}.${valueAfterComma}`;
-        } else {
-          numBeforeComma = `${valueBeforeComma}${valueAfterComma.split("")[0]}`;
-          const numAfterComma = `${valueAfterComma.split("")[1]}${
-            valueAfterComma.split("")[2]
-          }`;
-          const numBeforeCommaFormatted = numBeforeComma
-            .toString()
-            .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
-          this.amountSold = `${numBeforeCommaFormatted}.${numAfterComma}`;
+function formatCurrency(value) {
+  if (value && value != 0) {
+    value = value.replace(/,/g, "");
+    if (+value) {
+      if (value.includes(".")) {
+        if (value.split(".")[1].length >= 0) {
+          const valueBeforeComma = value.split(".")[0];
+          const valueAfterComma = value.split(".")[1];
+          let numBeforeComma;
+          if (valueAfterComma.length <= 2) {
+            const numBeforeComma = value
+              .split(".")[0]
+              .toString()
+              .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+            return `${numBeforeComma}.${valueAfterComma}`;
+          } else {
+            numBeforeComma = `${valueBeforeComma}${
+              valueAfterComma.split("")[0]
+            }`;
+            const numAfterComma = `${valueAfterComma.split("")[1]}${
+              valueAfterComma.split("")[2]
+            }`;
+            const numBeforeCommaFormatted = numBeforeComma
+              .toString()
+              .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
+            return `${numBeforeCommaFormatted}.${numAfterComma}`;
+          }
         }
+      } else {
+        return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
       }
-    } else {
-      this.amountSold = value
-        .toString()
-        .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
     }
-  } else {
-    this.amountSold = "";
   }
 }
